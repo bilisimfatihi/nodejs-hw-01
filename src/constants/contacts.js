fafe2d25b3708e8fe03dd0f6b5fe37ec5126241a
bilisimfatihi <bilisimfatihi@gmail.com>
2025-09-18 00:54:17 +0300

    kodlar eklendi

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+import path from 'node:path';
+
+const pathToWorkDir = path.join(process.cwd());
+const dbPath = path.join(pathToWorkDir, 'src/db', 'db.json');
+
+export const PATH_DB = dbPath;
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const text = await fs.readFile(PATH_DB, 'utf-8');
+    if (!text.trim()) return [];
+    const data = JSON.parse(text);
+    return data;
+  } catch (err) {
+    console.error('Dosya okuma hatası:', err);
+  }
+};
