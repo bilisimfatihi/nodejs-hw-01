58b3e8624c360561366d76b9a109868bf9abb78a
bilisimfatihi <bilisimfatihi@gmail.com>
2025-09-18 21:52:25 +0300

    generate command fix

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,11 +1,11 @@
-import { createFakeContact } from '../utils/createFakeContact';
-import { readContacts } from '../utils/readContacts';
-import { writeContacts } from '../utils/writeContacts';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
 const generateContacts = async (number) => {
   const contacts = await readContacts();
 
-  for (let i = 0; index < number; i++) {
+  for (let i = 0; i < number; i++) {
     contacts.push(createFakeContact());
   }
 
